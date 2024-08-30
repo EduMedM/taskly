3fcf7fd90cdbbb9660b49a413914b3107454453f
EduMedM <edumedm@gmail.com>
2024-08-30 11:49:57 -0400

    Board Server Action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,17 +1,21 @@
-import { db } from "@/lib/db";
-import { Board } from "./board";
-import { Form } from "./form";
+import { Suspense } from "react";
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
+//import { checkSubscription } from "@/lib/subscription";
 
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
+  //const isPro = await checkSubscription();
 
   return (
-    <div className="flex flex-col space-y-4">
-      <Form></Form>
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id}></Board>
-        ))}
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+        {/* <Suspense fallback={<BoardList.Skeleton />}>
+          
+        </Suspense> */}
       </div>
     </div>
   );
