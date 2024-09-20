70ccec950d761398b3e0aadd700bb9948ff97e78
EduMedM <39465077+EduMedM@users.noreply.github.com>
2024-09-20 13:16:45 -0400

    Update page.tsx

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -2,10 +2,8 @@ import { Suspense } from "react";
 import { Separator } from "@/components/ui/separator";
 import { Info } from "./_components/info";
 import { BoardList } from "./_components/board-list";
-//import { checkSubscription } from "@/lib/subscription";
 
 const OrganizationIdPage = async () => {
-  //const isPro = await checkSubscription();
 
   return (
     <div className="w-full mb-20">
